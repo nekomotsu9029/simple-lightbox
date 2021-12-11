f5d1c02d85b475ed16fefdeebcf90729e8ab0854
nekomotsu9029 <gustavo.olmedo@softwareestrategico.com>
2021-12-11 12:04:05 -0500

    ya tiene la opcion de album pero me falta agregar la funcionalidad de agrupar albums separados tipo este conjunto es algum-1 y este otro album-2

--- simple_lightbox.js
+++ simple_lightbox.js
@@ -5,13 +5,16 @@ let htmlModal = `
     .lightbox {
       cursor: pointer;
     }
+    .d-none {
+        display: none !important;
+    }
     .no_scroll {
       overflow-y: hidden !important;
       overflow-x: hidden !important;
     }
     #lightbox_nekomotsu {
         display: none;
-        position: absolute;
+        position: fixed;
         padding-bottom: 50px;
         left: 0;
         top: 0;
@@ -53,22 +56,43 @@ let htmlModal = `
             max-width: 80% !important;
         }
      }
-    /* width */
+     ::-webkit-scrollbar:horizontal {
+        height: 5px;
+    }
     ::-webkit-scrollbar {
         width: 5px;
       }
       /* Track */
       ::-webkit-scrollbar-track {
-        background: #f1f1f1; 
+        background: #888;
       }
       /* Handle */
       ::-webkit-scrollbar-thumb {
-        background: #888; 
+        background: #555; 
       }
       /* Handle on hover */
       ::-webkit-scrollbar-thumb:hover {
-        background: #555; 
+        background: #f1f1f1;
       }
+    #preview-album_lightbox_nekomotsu {
+        position: fixed;
+        width: 100%;
+        height: 105px;
+        background-color: rgba(0, 0, 0, 0.459);
+        padding: 5px;
+        bottom: 0px;
+        left: 0px;
+        display: flex;
+        justify-content: left;
+        overflow-x: scroll;
+    }
+    #preview-album_lightbox_nekomotsu > img {
+        width: auto;
+        height: 90px !important;
+        margin-right: 5px;
+        margin-left: 5px;
+        border: 2px solid gray;
+    }
     </style>
     <div id="lightbox_nekomotsu" class="animate__animated">
         <img id="img_lightbox_nekomotsu" class="animate__animated" src="" />
@@ -78,40 +102,45 @@ let htmlModal = `
 document.querySelector('body').innerHTML += htmlModal;
 }
 
-document.querySelector('#lightbox_nekomotsu').addEventListener('click', function (ev) {
-    ev.stopPropagation();
-    document.querySelector('body').classList.remove('no_scroll');
-    document.querySelector('#lightbox_nekomotsu').classList.remove('animate__fadeIn')
-    document.querySelector('#lightbox_nekomotsu').classList.add('animate__fadeOut')
-    setTimeout(function () {
-        document.querySelector('#lightbox_nekomotsu').classList.remove('active')
-    }, 500)
-})
+function lightbox(url, album) {
+    if(!document.querySelector('#lightbox_nekomotsu').classList.contains('active')){
+        document.querySelector('#lightbox_nekomotsu').innerHTML = `<img id="img_lightbox_nekomotsu" class="animate__animated" src="${url}" />`
+        
+        if(album){
+            document.querySelector('#lightbox_nekomotsu').innerHTML += `<div id="preview-album_lightbox_nekomotsu"></div>`
 
-document.querySelector('#img_lightbox_nekomotsu').addEventListener('click', function (ev) {
-    ev.stopPropagation();
-})
+            document.querySelectorAll('.lightbox-album').forEach(function(element){
+                document.querySelector('#preview-album_lightbox_nekomotsu').innerHTML += `<img src="${element.src}" class="lightbox lightbox-album lightbox-preview" />`
+            })
 
-function lightbox(url) {
-    document.querySelector('#lightbox_nekomotsu').innerHTML = `<img id="img_lightbox_nekomotsu" class="animate__animated" src="${url}" />`
-    document.querySelector('body').classList.add('no_scroll');
-    document.querySelector('#lightbox_nekomotsu').style.top = window.scrollY+'px';
-    document.querySelector('#lightbox_nekomotsu').classList.remove('animate__fadeOut')
-    document.querySelector('#lightbox_nekomotsu').classList.add('active', 'animate__fadeIn')
-    document.querySelector('#img_lightbox_nekomotsu').classList.add('active', 'animate__fadeInDown')
+        }
+        
+        document.querySelector('body').classList.add('no_scroll');
+        document.querySelector('#lightbox_nekomotsu').classList.remove('animate__fadeOut')
+        document.querySelector('#lightbox_nekomotsu').classList.add('active', 'animate__fadeIn')
+        document.querySelector('#img_lightbox_nekomotsu').classList.add('active', 'animate__fadeInDown')
+
+        document.querySelectorAll('img').forEach(function(element){
+            element.addEventListener('click', function (ev) {
+                if(element.classList.contains('lightbox-preview')){
+                    lightbox(element.src, element.classList.contains('lightbox-album'));
+                }
+                ev.stopPropagation();
+            })
+        })
+    }else{
+        document.querySelector('#img_lightbox_nekomotsu').src = url;
+    }
+    
     let ancho = document.querySelector('#img_lightbox_nekomotsu').width;
     let alto = document.querySelector('#img_lightbox_nekomotsu').height;
+    
     if(ancho>alto){
         document.querySelector('#img_lightbox_nekomotsu').classList.add('img_x')
+    }else{
+        document.querySelector('#img_lightbox_nekomotsu').classList.remove('img_x')
     }
-    console.log('el ancho es: '+document.querySelector('#img_lightbox_nekomotsu').width)
 }
-
-document.querySelectorAll('img.lightbox').forEach(function (element) {
-    element.addEventListener('click', function () {
-        lightbox(element.src);
-    })
-});
 
 function lightbox_video(url){
     document.querySelector('#lightbox_nekomotsu').innerHTML = `<video id="video_lightbox_nekomotsu" controls class="animate__animated">
@@ -124,3 +153,19 @@ function lightbox_video(url){
     document.querySelector('#video_lightbox_nekomotsu').classList.add('active', 'animate__fadeInDown')
 
 }
+
+document.querySelector('#lightbox_nekomotsu').addEventListener('click', function (ev) {
+    ev.stopPropagation();
+    document.querySelector('body').classList.remove('no_scroll');
+    document.querySelector('#lightbox_nekomotsu').classList.remove('animate__fadeIn')
+    document.querySelector('#lightbox_nekomotsu').classList.add('animate__fadeOut')
+    setTimeout(function () {
+        document.querySelector('#lightbox_nekomotsu').classList.remove('active')
+    }, 500)
+})
+
+document.querySelectorAll('img.lightbox').forEach(function (element) {
+    element.addEventListener('click', function () {
+        lightbox(element.src, element.classList.contains('lightbox-album'));
+    })
+});
